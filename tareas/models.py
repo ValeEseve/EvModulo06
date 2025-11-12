e2d34d8000731800e67bce10090ec52103033e82
Vale Eseve <vale.eseve@gmail.com>
2025-11-11 21:26:04 -0300

    Modificado modelo Tarea, añadido color, prioridad, fechas

--- tareas/models.py
+++ tareas/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
+
+
 # Create your models here.
 class Tarea(models.Model):
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField()
+    categoria = models.CharField(max_length=100, default='General')
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_vencimiento = models.DateTimeField(null=True, blank=True)
+    prioridad = models.IntegerField(default=1)
+    color_etiqueta = models.CharField(max_length=7, default='#FFFFFF')
     completada = models.BooleanField(default=False)
 
     def __str__(self):
