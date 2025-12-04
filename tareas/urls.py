96b180b367f85ed42501b6075618180447b0ae4f
Vale Eseve <vale.eseve@gmail.com>
2025-12-04 15:55:10 -0300

    arregladas rutas a tareas/**

--- tareas/urls.py
+++ tareas/urls.py
@@ -1,9 +1,9 @@
-from os import path
+from django.urls import path
 from tareas import views
 
 
 urlpatterns = [
-    path('', views.lista_tareas, name='dashboard'),
+    path('', views.home, name='home'),
     path('tarea/<int:id>/', views.detalle_tarea, name='detalle_tarea'),
     path('tarea/nueva/', views.nueva_tarea, name='nueva_tarea'),
     path('tarea/eliminar/<int:id>/', views.eliminar_tarea, name='eliminar_tarea'),
--- tareas/views.py
+++ tareas/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
-
 from tareas.models import Tarea
 
 def home(request):
