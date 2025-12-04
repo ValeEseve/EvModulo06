47b882a66ea62742ab2c8ef9729abf297414cc80
Vale Eseve <vale.eseve@gmail.com>
2025-12-04 16:21:15 -0300

    migraciones hechas - actualización modelo Tarea

--- tareas/forms.py
+++ tareas/forms.py
@@ -4,7 +4,7 @@ from tareas.models import Tarea
 class TareaForm(forms.ModelForm):
     class Meta:
         model = Tarea
-        fields = ['nombre', 'descripcion', 'prioridad', 'fecha_vencimiento']
+        fields = ['titulo', 'descripcion', 'categoria', 'prioridad', 'fecha_vencimiento']
         widgets = {
             'fecha_vencimiento': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
