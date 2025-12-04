50458d6e9be3a65f4096fd79fb2f496e1eb96db4
Vale Eseve <vale.eseve@gmail.com>
2025-12-04 14:04:58 -0300

    modificado modelo Tarea para aceptar opciones de prioridad

--- tareas/models.py
+++ tareas/models.py
@@ -1,15 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-
-
-# Create your models here.
 class Tarea(models.Model):
+    # Se establecen las opciones de prioridad para las tareas
+    PRIORIDAD_CHOICES = [
+        (1, 'Baja'),
+        (2, 'Media'),
+        (3, 'Alta'),
+        (4, 'Urgente'),
+    ]
+    # Llamar al modelo User para relacionar cada tarea con un usuario específico
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tareas')
+    
+    # Atributos de cada tarea
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField()
     categoria = models.CharField(max_length=100, default='General')
+    #fechas
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_vencimiento = models.DateTimeField(null=True, blank=True)
-    prioridad = models.IntegerField(default=1)
+
+    prioridad = models.IntegerField(default=1, choices=PRIORIDAD_CHOICES)
     color_etiqueta = models.CharField(max_length=7, default='#FFFFFF')
     completada = models.BooleanField(default=False)
 
