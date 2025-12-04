77abb87832aab46912e46b9f0fce71bb2916fc14
Vale Eseve <vale.eseve@gmail.com>
2025-12-04 14:35:10 -0300

    Agregadas nuevas vistas y templates para home, login y logout

--- tareas/urls.py
+++ tareas/urls.py
@@ -3,7 +3,7 @@ from tareas import views
 
 
 urlpatterns = [
-    path('', views.lista_tareas, name='lista_tareas'),
+    path('', views.lista_tareas, name='dashboard'),
     path('tarea/<int:id>/', views.detalle_tarea, name='detalle_tarea'),
     path('tarea/nueva/', views.nueva_tarea, name='nueva_tarea'),
     path('tarea/eliminar/<int:id>/', views.eliminar_tarea, name='eliminar_tarea'),
--- tareas/views.py
+++ tareas/views.py
@@ -1,23 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 
 from tareas.models import Tarea
 
-
-# Vistas Auth
 def home(request):
-    return render(request, 'tareas/dashboard.html')
+    return render(request, 'tareas/home.html')
+
+# Vistas para autenticación
+def login_view(request):
+    pass
+
+def logout_view(request):
+    pass
 
 
 
 @login_required
 def lista_tareas(request):
     tareas = Tarea.objects.filter(usuario=request.user)
-    return render(request, 'tareas/lista_tareas.html', {'tareas': tareas})
+    return render(request, 'tareas/dashboard.html', {'tareas': tareas})
 
 @login_required
 def detalle_tarea(request, id):
-    pass
+    tarea = get_object_or_404(Tarea, id=id, usuario=request.user)
+    return render(request, 'tareas/detalle_tarea.html', {'tarea': tarea})
 
 @login_required
 def nueva_tarea(request):
